998c5d3706382cf1e86901865df33246e7f7ac4f
MiguelNiblock <miguelniblock@posteo.net>
2020-06-22 15:48:45 -0700

    wireframes and initial data model

---
+++ QAdata.js
@@ -0,0 +1,80 @@
+QAdata = [
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+    {question : 
+        `What is coronavirus disease?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswer : 1},
+]
