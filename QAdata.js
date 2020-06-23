3933da63f9ad4262601f6563cdcda09b8f777ba2
MiguelNiblock <miguelniblock@posteo.net>
2020-06-23 13:49:39 -0700

    features progress

--- QAdata.js
+++ QAdata.js
@@ -11,9 +11,22 @@ QAdata = [
         {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
         response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
     ],
-    correctanswer : 1},
+    correctanswerId : 1},
     {question : 
-        `What is coronavirus disease?`,
+        `What is coronavirus disease2?`,
+    answers : [
+        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
+        response:`You watch too many conspiracy theory documentaries`},
+        {answer:`An infectious disease caused by a newly discovered coronavirus`,
+        response:`You got it! You're on top of things. Congratulations`},
+        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
+        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
+        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
+        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswerId : 1},
+    {question : 
+        `What is coronavirus disease3?`,
     answers : [
         {answer:`A fake myth invented with the goal of depriving people of their liberty`,
         response:`You watch too many conspiracy theory documentaries`},
@@ -26,20 +39,7 @@ QAdata = [
     ],
     correctanswer : 1},
     {question : 
-        `What is coronavirus disease?`,
-    answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
-    ],
-    correctanswer : 1},
-    {question : 
-        `What is coronavirus disease?`,
+        `What is coronavirus disease4?`,
     answers : [
         {answer:`A fake myth invented with the goal of depriving people of their liberty`,
         response:`You watch too many conspiracy theory documentaries`},
--- index.js
+++ index.js
@@ -27,6 +27,10 @@ function nextButtonListener(){
         console.log('Loading next...')
 
         questionCount++;
+        $('nav span.questionCount').text(questionCount)
+        $('nav span.correctCount').text(correctCount)
+        $('nav span.incorrectCount').text(incorrectCount)
+
 
         if (questionCount<11) {
             console.log('Current question: '+questionCount)
@@ -58,22 +62,51 @@ function nextButtonListener(){
     })
 }
 
+function feedbackMessage(questionCount,isCorrect,answerSubmittedId){
+    let response = QAdata[questionCount-1].answers[answerSubmittedId].response
+
+    console.log('Response: ' + response)
+
+    if (isCorrect){
+        return `<p>Correct!</p>
+                <p>${response}</p>`
+    }
+    else {
+        let correctAnsId = QAdata[questionCount-1].correctanswerId
+        let correctAnswer = QAdata[questionCount-1].answers[correctAnsId].answer
+        
+        return `<p>Incorrect!</p>
+                <p>${response}</p>
+                <p>The correct answer is:</p>
+                <p>${correctAnswer}</p>
+                `
+    }
+}
+
 function submitListener(){
     console.log('listening to Submit button')
     $('button.submit').on('click',function(e){
         
         e.preventDefault();
-        console.log('Answer submitted')
 
-        $('.output').html(`
-            <p>if (right){"You know your stuff! Congrats!"} else {"We're sorry but you missed"}.</p>
-            <p class="reveal-correct">if (incorrect){The correct answer is: }</p>
-            <p><span>'Correct answer here'</span></p>
+        let answerSubmittedId = parseInt($('input[name="answer"]:checked').val())
+        console.log('Answer submitted: '+ answerSubmittedId)
+
+        let isCorrect = answerSubmittedId === QAdata[questionCount].correctanswerId
+
+        console.log('Answer is correct: '+isCorrect)
+        if (isCorrect){correctCount++;}
+        else{incorrectCount++;}
+
+        $('nav span.correctCount').text(correctCount)
+        $('nav span.incorrectCount').text(incorrectCount)
+
+        $('section.output').html(`
+            ${feedbackMessage(questionCount,isCorrect,answerSubmittedId)}
+            <button type="submit" class="next" name="Next">Next</button>
         `)
 
-        $('button.submit').toggleClass('submit next')
-        $('button.next').attr('name','Next')
-        $('button.next').text('Next')
+        $('button.submit').toggleClass('hidden')
 
         nextButtonListener();
     })
@@ -84,6 +117,8 @@ function startButtonListener(){
     console.log('listening to start button')
     
     questionCount = 0;
+    correctCount = 0;
+    incorrectCount = 0;
     
     $('button.start').on('click',function(e){
 
@@ -93,19 +128,23 @@ function startButtonListener(){
         console.log('current question: ' + questionCount)
 
         $('nav').removeClass('hidden')
+        $('nav span.questionCount').text(questionCount)
+        $('nav span.correctCount').text(correctCount)
+        $('nav span.incorrectCount').text(incorrectCount)
+
         $('section.output').html('')
         $('section.QA').html(`
         <form>
             <fieldset>
-                <legend>How are you?</legend>
-                <input type="radio" id="ans1" name="answer">
-                <label for="ans1">Answer</label><br>
-                <input type="radio" id="ans2" name="answer">
-                <label for="ans2">Answer</label><br>
-                <input type="radio" id="ans3" name="answer">
-                <label for="ans3">Answer</label><br>
-                <input type="radio" id="ans4" name="answer">
-                <label for="ans4">Answer</label><br>
+                <legend>${QAdata[questionCount-1].question}</legend>
+                <input type="radio" id="ans1" name="answer" value=0>
+                <label for="ans1">${QAdata[questionCount-1].answers[0].answer}</label><br>
+                <input type="radio" id="ans2" name="answer" value=1>
+                <label for="ans2">${QAdata[questionCount-1].answers[1].answer}</label><br>
+                <input type="radio" id="ans3" name="answer" value=2>
+                <label for="ans3">${QAdata[questionCount-1].answers[2].answer}</label><br>
+                <input type="radio" id="ans4" name="answer" value=3>
+                <label for="ans4">${QAdata[questionCount-1].answers[3].answer}</label><br>
             </fieldset>
             <button type="submit" class="submit" name="Submit">Submit</button>
         </form>
