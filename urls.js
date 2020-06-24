680a5157bc03ef00baba05937a8d9a07aa69ae8a
MiguelNiblock <miguelniblock@posteo.net>
2020-06-23 17:38:57 -0700

    added media

---
+++ urls.js
@@ -0,0 +1,4 @@
+RESOURCES = {
+    background : "https://static.politico.com/b6/64/b13e1e86467fa4233784ed18bb64/politico-friday-5-color.jpg",
+    singleVirus : "https://dlg7f0e93aole.cloudfront.net/wp-content/uploads/coronavirus-image-scaled-e1580223034877.jpg"
+}
