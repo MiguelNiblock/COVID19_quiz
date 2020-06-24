927c8fd913c958ee0b77957d008bbec878d2ff9b
MiguelNiblock <miguelniblock@posteo.net>
2020-06-23 17:05:32 -0700

    updated questions and answers

--- QAdata.js
+++ QAdata.js
@@ -13,120 +13,120 @@ QAdata = [
     ],
     correctanswerId : 1},
     {question : 
-        `What is coronavirus disease2?`,
+        `How does COVID-19 virus spread?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`You get it if you have Chinese friends.`,
+        response:`You should be ashamed of yourself for thinking this.`},
+        {answer:`Spreads because people give it too much importance. You attract what you put out there.`,
+        response:`Yeah... no. Unfortunately you can get it whether you're aware of it or not.`},
+        {answer:`Through droplets of saliva or discharge from the nose when an infected person coughs or sneezes,`,
+        response:`Yes! Through any fluids that emanate from the face.`},
+        {answer:`Through contaminated food and water.`,
+        response:`This might actually sound true if you hadn't ever heard of it. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+    ],
+    correctanswerId : 2},
+    {question : 
+        `What are the most common symptoms of coronavirus disease?`,
+    answers : [
+        {answer:`Aches and pains, nasal congestion, headache.`,
+        response:`These are less common.`},
+        {answer:`Conjunctivitis, sore throat, diarrhea, loss of taste or smell or a rash on skin or discoloration of fingers or toes.`,
+        response:`These are very rare symptoms.`},
+        {answer:`Unexplained hemorrhaging, bleeding or bruising.`,
+        response:`These are Ebola symptoms!`},
+        {answer:`Fever, dry cough, and tiredness.`,
+        response:`These are the most common symptoms, although answers 1 and 2 are also less common symptoms.`}
+    ],
+    correctanswerId : 3},
+    {question : 
+        `What proportion of COVID-19 positive people become seriously ill and develop difficulty breathing?`,
+    answers : [
+        {answer:`1 out of every 5.`,
+        response:`Look at you! You're an expert.`},
+        {answer:`3 out of every 5.`,
+        response:`That would be too many...`},
+        {answer:`4 out of every 5.`,
+        response:`If this were true, the world population would significantly decline in the next year...`},
+        {answer:`2 out of every 5.`,
+        response:`You were close...`}
+    ],
+    correctanswerId : 0},
+    {question : 
+        `When should you seek medical care?`,
+    answers : [
+        {answer:`If you have minor symptoms, such as a slight cough or a mild fever.`,
+        response:`In this case you should stay at home, self-isolate, and monitor your symptoms.`},
+        {answer:`If you have difficulty breathing or pain/pressure in the chest.`,
+        response:`If possible, call your health care provider in advance, so he/she can direct you to the right health facility.`},
+        {answer:`As soon as possible after having contact with someone who is infected.`,
+        response:`This doesn't guarantee that you'll be infected, but you should self-quarantine in case you are already contagious without developing symptoms.`},
+        {answer:`Never. I can cure myself with the power of my mind and my supercharged crystals.`,
+        response:`Knock yourself by all means. You can always change your mind when it gets rough.`}
     ],
     correctanswerId : 1},
     {question : 
-        `What is coronavirus disease3?`,
+        `Can children and adolescents catch COVID-19?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`No, they're immune.`,
+        response:`Evidence to date suggests that children and young adults are less likely to get severe disease, but severe cases can still happen in these age groups. `},
+        {answer:`Yes, they're just as likely as any other age group to get it.`,
+        response:`Evidence to date suggests that children and young adults are less likely to get severe disease, but severe cases can still happen in these age groups. `},
+        {answer:`They're less likely to become infected.`,
+        response:`Evidence to date suggests that children and young adults are less likely to get severe disease, but severe cases can still happen in these age groups. `},
+        {answer:`They're more likely to become infected.`,
+        response:`Evidence to date suggests that children and young adults are less likely to get severe disease, but severe cases can still happen in these age groups. `}
     ],
     correctanswerId : 1},
     {question : 
-        `What is coronavirus disease4?`,
+        `Should you wear a mask while exercising outdoors?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`Yes, since you can get COVID in the air if someone sneezes.`,
+        response:`Sweat can make the mask become wet more quickly which makes it difficult to breathe and promotes the growth of microorganisms. The important preventive measure during exercise is to maintain physical distance of at least one meter from others. `},
+        {answer:`No. It's not really beneficial.`,
+        response:`Sweat can make the mask become wet more quickly which makes it difficult to breathe and promotes the growth of microorganisms. The important preventive measure during exercise is to maintain physical distance of at least one meter from others. `},
+        {answer:`Only if you're running. But not if you're riding a bicycle.`,
+        response:`Sweat can make the mask become wet more quickly which makes it difficult to breathe and promotes the growth of microorganisms. The important preventive measure during exercise is to maintain physical distance of at least one meter from others. `},
+        {answer:`Only if it's summer time. Winter brings COVID`,
+        response:`Sweat can make the mask become wet more quickly which makes it difficult to breathe and promotes the growth of microorganisms. The important preventive measure during exercise is to maintain physical distance of at least one meter from others. `}
     ],
     correctanswerId : 1},
     {question : 
-        `What is coronavirus disease5?`,
+        `Can you get COVID-19 in the Summer?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`No. Like other viruses, it can't live at temperatures higher than 25C degrees.`,
+        response:`You can catch COVID-19, no matter how sunny or hot the weather is. Countries with hot weather have reported cases of COVID-19.`},
+        {answer:`Only if you live near the arctic pole, since winters are longer at these locations.`,
+        response:`You can catch COVID-19, no matter how sunny or hot the weather is. Countries with hot weather have reported cases of COVID-19.`},
+        {answer:`Temperature and location make no difference.`,
+        response:`You can catch COVID-19, no matter how sunny or hot the weather is. Countries with hot weather have reported cases of COVID-19.`},
+        {answer:`Yes but it's less likely.`,
+        response:`You can catch COVID-19, no matter how sunny or hot the weather is. Countries with hot weather have reported cases of COVID-19.`}
     ],
-    correctanswerId : 1},
+    correctanswerId : 2},
     {question : 
-        `What is coronavirus disease6?`,
+        `How long does COVID survive on surfaces?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`It dies immediately after leaving your nose/ mouth.`,
+        response:`Studies have shown that the COVID-19 virus can survive for up to 72 hours on plastic and stainless steel, less than 4 hours on copper and less than 24 hours on cardboard. `},
+        {answer:`It only survives for about 5 minutes outside the body.`,
+        response:`Studies have shown that the COVID-19 virus can survive for up to 72 hours on plastic and stainless steel, less than 4 hours on copper and less than 24 hours on cardboard. `},
+        {answer:`It can survive for up to 24 hours anywhere outside the body.`,
+        response:`Studies have shown that the COVID-19 virus can survive for up to 72 hours on plastic and stainless steel, less than 4 hours on copper and less than 24 hours on cardboard. `},
+        {answer:`Depends on the surface material.`,
+        response:`Studies have shown that the COVID-19 virus can survive for up to 72 hours on plastic and stainless steel, less than 4 hours on copper and less than 24 hours on cardboard. `}
     ],
-    correctanswerId : 1},
+    correctanswerId : 3},
     {question : 
-        `What is coronavirus disease7?`,
+        `Is it safe to have a normal life now that businesses are reopening?`,
     answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
+        {answer:`The government knows what's best, and if they are allowing businesses to operate, it must be safe.`,
+        response:`Governments are simply trying to save the economy and leaving it up to individuals to choose for themselves what to do.`},
+        {answer:`Yes because it's not winter anymore.`,
+        response:`Governments are simply trying to save the economy and leaving it up to individuals to choose for themselves what to do.`},
+        {answer:`No, COVID hasn't gone anywhere, and the worst might be yet to come.`,
+        response:`Governments are simply trying to save the economy and leaving it up to individuals to choose for themselves what to do.`},
+        {answer:`It's somehwat safer than in March. Much of it has been controlled.`,
+        response:`Governments are simply trying to save the economy and leaving it up to individuals to choose for themselves what to do.`}
     ],
-    correctanswerId : 1},
-    {question : 
-        `What is coronavirus disease8?`,
-    answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
-    ],
-    correctanswerId : 1},
-    {question : 
-        `What is coronavirus disease9?`,
-    answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
-    ],
-    correctanswerId : 1},
-    {question : 
-        `What is coronavirus disease10?`,
-    answers : [
-        {answer:`A fake myth invented with the goal of depriving people of their liberty`,
-        response:`You watch too many conspiracy theory documentaries`},
-        {answer:`An infectious disease caused by a newly discovered coronavirus`,
-        response:`You got it! You're on top of things. Congratulations`},
-        {answer:`A disease caused by excesively drinking corona beer. (The Mexican beer company that just closed after infecting the whole world)`,
-        response:`LOL You can't be seriously thinking this. But it's a funny answer`},
-        {answer:`A disease experienced by royal families who wear crowns. (Corona means Crown in Spanish)`,
-        response:`This might actually sound true if you were living inside a hole since the end of 2019. Do not feel ashamed though; By the end of this test you'll be all caught up!`}
-    ],
-    correctanswerId : 1},
+    correctanswerId : 2},
 ]
